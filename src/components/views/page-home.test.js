0f672f6bb5b4fde2615cb1e82699b7daac782efd
EstherGodoy <info@esthergodoy.com>
2020-08-11 10:52:15 -0700

    mid testing

--- src/components/views/page-home.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PageHome from './page-home';
-
-it('component renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PageHome store={test} />, div);
-});
